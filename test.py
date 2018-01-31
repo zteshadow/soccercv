c9ccbd24f84c7b8cc8c63cafcf130a7bddb8ae5f
samuel <songliantao@baidu.com>
2018-01-31 19:23:25 +0800

    resize image to show

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import imutils
 import cv2
 import numpy as np
 
+# load image
 image = cv2.imread('./1.jpg', 0)
 
 # Initiate SIFT detector
@@ -11,7 +12,11 @@ detector = cv2.xfeatures2d.SIFT_create()
 kps, descriptor = detector.detectAndCompute(image, None)
 
 # draw key points into feature_image
-feature_image = cv2.drawKeypoints(image, kps, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+feature_image = cv2.drawKeypoints(image, kps, None, color = (0, 0, 255), flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# resize image for show if needed
+if feature_image.shape[1] > 800:
+  feature_image = imutils.resize(feature_image, width = 800)
 
 # show feature image
 cv2.imshow("feature image", feature_image)
