cfabbdbdd65717994f0c16a24c03ba5fd79c5058
samuel <songliantao@baidu.com>
2018-04-15 09:02:24 +0800

    demo done

---
+++ opencvstudy/cameo.py
@@ -0,0 +1,38 @@
+
+import cv2
+from managers import WindowManager, CaptureManager
+
+class Cameo(object):
+    def __init__(self):
+        self._window = WindowManager('Cameo', self.onKeypress)
+        self._capture = CaptureManager(cv2.VideoCapture(0), self._window, True)
+
+    def run(self):
+        self._window.createWindow()
+        while self._window.isWindowCreated:
+            self._capture.enterFrame()
+            frame = self._capture.frame
+            self._window.show(frame)
+            self._capture.exitFrame()
+            self._window.processEvents()
+
+    def onKeypress(self, keycode):
+        """
+        space -> take a snapshot
+        tab ->start/stop recording a screencast
+        escape -> Quit
+        """
+        if keycode == 32: #space
+            self._capture.writeImage('screenshot.jpg')
+        elif keycode == 9: #tab
+            if not self._capture.isWritingVideo:
+                self._capture.startWritingVideo('screenshot.avi')
+            else:
+                self._capture.stopWritingVideo()
+        elif keycode == 27: #esc
+            self._window.destroyWindow()
+
+if __name__ == '__main__':
+    Cameo().run()
+
+
--- opencvstudy/managers.py
+++ opencvstudy/managers.py
@@ -2,6 +2,7 @@
 import cv2
 import numpy
 import time
+import os
 
 class CaptureManager(object):
     def __init__(self, capture, window = None, mirror = False):
@@ -20,7 +21,7 @@ class CaptureManager(object):
 
         #estimate the fts by _frameElapsed / time
         self._startTime = None
-        self._frameElapsed = long(0)
+        self._frameElapsed = int(0)
         self._fptEstimate = None
 
     @property
@@ -49,7 +50,7 @@ class CaptureManager(object):
     
     def enterFrame(self):
         """Capture the next frame if any"""
-        assert not self.enterFrame, "previous enter frame had no matching exit frame"
+        assert not self._enteredFrame, "previous enter frame had no matching exit frame"
 
         if self._capture is not None:
             self._enteredFrame = self._capture.grab()
@@ -86,4 +87,56 @@ class CaptureManager(object):
 
     def writeImage(self, fname):
         self._imageFileName = fname
-        
+
+    def startWritingVideo(self, fname, encoding = cv2.VideoWriter_fourcc('I', '4', '2', '0')):
+        self._videoFileName = fname
+        self._videoEncoding = encoding
+
+    def stopWritingVideo(self):
+        self._videoFileName = None
+        self._videoEncoding = None
+        self._videoWriter= None
+
+    def _writeVideoFrame(self):
+        if not self.isWritingVideo:
+            return
+        if self._videoWriter is None:
+            fps = self._capture.get(cv2.CAP_PROP_FPS)
+            if fps == 0.0: #fps is unkonwn, we need a estimate
+                if self._frameElapsed < 20: #need more frame to get a stable fps
+                    return
+                else:
+                    fps = self._fptEstimate
+            size = (int(self._capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self._capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+            self._videoWriter = cv2.VideoWriter(self._videoFileName, self._videoEncoding, fps, size)
+        self._videoWriter.write(self._frame)
+
+class WindowManager(object):
+    def __init__(self, name, keyCallback = None):
+        self.keyCallback = keyCallback
+        self._name = name
+        self._isWindowCreated = False
+
+    @property
+    def isWindowCreated(self):
+        return self._isWindowCreated
+    
+    def createWindow(self):
+        cv2.namedWindow(self._name)
+        self._isWindowCreated = True
+
+    def show(self, frame):
+        cv2.imshow(self._name, frame)
+
+    def destroyWindow(self):
+        cv2.destroyWindow(self._name)
+        self._isWindowCreated = False
+
+    def processEvents(self):
+        keycode = cv2.waitKey(1)
+        if self.keyCallback is not None and keycode != -1:
+            keycode &= 0xFF
+            self.keyCallback(keycode)
+
+
+
