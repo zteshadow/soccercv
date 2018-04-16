ff107a20c3e2141e9ed7efae84256abff50b1d4d
zteshadow <samuel.song.bc@gmail.com>
2018-04-16 19:15:39 +0800

    hpf test

--- opencvstudy/test.py
+++ opencvstudy/test.py
@@ -1,7 +1,9 @@
 
+"""
 import numpy
 import cv2
 import os
+
 
 click = False
 def onMouse(event, x, y, flag, parm):
@@ -20,3 +22,27 @@ while sucess and cv2.waitKey(1) == -1 and not click:
 
 cv2.destroyWindow('MyWindow')
 camera.release
+
+"""
+
+import cv2
+import numpy as np
+from scipy import ndimage
+
+kernel_3x3 = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+kernel_5x5 = np.array([[-1, -1, -1, -1, -1], [-1, 1, 2, 1, -1], [-1, 2, 4, 2, -1], [-1, 1, 2, 1, -1], [-1, -1, -1, -1, -1]])
+
+image = cv2.imread('./my.jpg', 0)
+
+k3 = ndimage.convolve(image, kernel_3x3)
+k5 = ndimage.convolve(image, kernel_5x5)
+blur = cv2.GaussianBlur(image, (11, 11), 0)
+hpf = image - blur
+
+cv2.imshow('3x3', k3)
+cv2.imshow('5x5', k5)
+cv2.imshow('hpf', hpf)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
+
