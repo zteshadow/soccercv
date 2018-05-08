4ec24059e5bb5eddd34d5c6ec36aece32005d69a
zteshadow <samuel.song.bc@gmail.com>
2018-05-08 10:14:17 +0800

    ok

--- tensorflow/test.py
+++ tensorflow/test.py
@@ -1,3 +1,25 @@
 
+import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+print(mnist.train.images.shape, mnist.train.labels.shape)
+
+session = tf.InteractiveSession()
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+cost = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cost)
+
+tf.global_variables_initializer().run()
+for i in range(1000):
+    batch_x, batch_y = mnist.train.next_batch(100)
+    train_step.run({x:batch_x, y_:batch_y})
+
+
