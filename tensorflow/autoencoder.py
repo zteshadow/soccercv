ca49ea94cc51d0dff2b36ac86faa66ceee8c891c
zteshadow <samuel.song.bc@gmail.com>
2018-05-09 19:39:06 +0800

    add test

--- tensorflow/autoencoder.py
+++ tensorflow/autoencoder.py
@@ -8,6 +8,34 @@ def xavier_init(fan_in, fan_out, constant = 1):
     high = constant * np.sqrt(6.0 / (fan_in + fan_out))
     return tf.random_uniform((fan_in, fan_out), minval=low, maxval=high, dtype=tf.float32)
 
+class AdditiveGaussianNoiseAutoencoder(object):
+    def __init__(self, n_input, n_hidden, transfer_function = tf.nn.softplus, optimizer = tf.train.AdamOptimizer(), scale = 0.1):
+        self.n_input = n_input
+        self.n_hidden = n_hidden
+        self.transfer = transfer_function
+        self.scale = tf.placeholder(tf.float32)
+        self.training_scale = scale
+        network_weights = self._initialize_weights()
+        self.weights = network_weights
+        self.x = tf.placeholder(tf.float32, [None, n_input])
+        self.hidden = self.transfer(tf.add(tf.matmul(self.x + scale * tf.random_normal((n_input,)), self.weights['w1']), self.weights['b1']))
+        self.reconstruct = tf.add(tf.matmul(self.hidden, self.weights['w2']), self.weights['b2'])
+        self.cost = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruct, self.x), 2.0))
+        self.optimizer = optimizer.minimize(self.cost)
+        init = rf.global_variables_initializer()
+        self.session = tf.Session()
+        self.session.run(init)
+        
+    def _initialize_weights(self):
+        all_weights = dict()
+        all_weights['w1'] = tf.Variable(xavier_init(self.n_input, self.n_hidden))
+        all_weights['b1'] = tf.Variable(tf.zeros([self.n_hidden], dtype = tf.float32))
+        all_weights['w2'] = tf.Variable(tf.zeros([self.n_hidden, self.n_input], dtype = tf.float32))
+        all_weights['b2'] = tf.Variable(tf.zeros([self.n_input]), dtype = tf.float32)
+        return all_weights
+        
+
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 print(mnist.train.images.shape, mnist.train.labels.shape)
 
