e9ef86519ffbe3809ed36d38b2d982d51df8f2f3
samuel <songliantao@baidu.com>
2018-05-11 06:59:33 +0800

    show cost

--- tensorflow/test.py
+++ tensorflow/test.py
@@ -18,10 +18,11 @@ cost = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cost)
 
 tf.global_variables_initializer().run()
-for i in range(1000):
+for i in range(100):
     batch_x, batch_y = mnist.train.next_batch(100)
     train_step.run({x:batch_x, y_:batch_y})
-
+    print('cost[', str(i), ']: ', session.run((cost), feed_dict={x:batch_x, y_:batch_y}))
+    #print('cost[', str(i), ']: ', tf.cast(cost, tf.float32))
 
 correct = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
