b71b3707d7506db0c479eb8ac6ca7a1ef1d2b192
samuel <songliantao@baidu.com>
2018-05-14 06:48:32 +0800

    with acuracy 97.97, amazing

--- tensorflow/test.py
+++ tensorflow/test.py
@@ -6,25 +6,34 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 print(mnist.train.images.shape, mnist.train.labels.shape)
 
 session = tf.InteractiveSession()
-x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros([10]))
 
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+#with one hidden layer
+in_units = 784
+h1_units = 300
+W1 = tf.Variable(tf.truncated_normal([in_units, h1_units], stddev=0.1))
+b1 = tf.Variable(tf.zeros([h1_units]))
+W2 = tf.Variable(tf.zeros([h1_units, 10]))
+b2 = tf.Variable(tf.zeros([10]))
+
+x = tf.placeholder(tf.float32, [None, in_units])
+keep_prob = tf.placeholder(dtype=tf.float32)
+hidden1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+hidden1_drop = tf.nn.dropout(hidden1, keep_prob)
+y = tf.nn.softmax(tf.matmul(hidden1_drop, W2) + b2)
 
 y_ = tf.placeholder(tf.float32, [None, 10])
 cost = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cost)
+train_step = tf.train.AdagradOptimizer(0.3).minimize(cost)
 
 tf.global_variables_initializer().run()
-for i in range(100):
+for i in range(3000):
     batch_x, batch_y = mnist.train.next_batch(100)
-    train_step.run({x:batch_x, y_:batch_y})
-    print('cost[', str(i), ']: ', session.run((cost), feed_dict={x:batch_x, y_:batch_y}))
+    train_step.run({x:batch_x, y_:batch_y, keep_prob:0.75})
+    print('cost[', str(i), ']: ', session.run((cost), feed_dict={x:batch_x, y_:batch_y, keep_prob:0.75}))
     #print('cost[', str(i), ']: ', tf.cast(cost, tf.float32))
 
 correct = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-print(accuracy.eval({x:mnist.test.images, y_:mnist.test.labels}))
+print(accuracy.eval({x:mnist.test.images, y_:mnist.test.labels, keep_prob:1.0}))
 
