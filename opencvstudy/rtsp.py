9d7195823246eb4582043080c65e631d595aa0f6
zteshadow <samuel.song.bc@gmail.com>
2018-07-24 07:00:06 +0800

    add rtsp player for Xiaoyi camera

---
+++ opencvstudy/rtsp.py
@@ -0,0 +1,15 @@
+
+import cv2
+
+#cap = cv2.VideoCapture("rtsp://admin:admin@192.168.2.64:554//Streaming/Channels/1")
+cap = cv2.VideoCapture(0)
+print(cap.isOpened())
+
+while cap.isOpened():
+    ret,frame = cap.read()
+    cv2.imshow("frame",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.release()
+cv2.destroyAllWindows()
