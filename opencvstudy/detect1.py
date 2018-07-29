ede09b8381bb6c83dc1839d0f0581025fe42fa3d
zteshadow <samuel.song.bc@gmail.com>
2018-07-30 05:53:59 +0800

    add detection test

---
+++ opencvstudy/detect1.py
@@ -0,0 +1,37 @@
+
+import cv2
+import numpy as np
+
+camera = cv2.VideoCapture(0)
+es = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 4))
+kernel = np.ones((5, 5), np.uint8)
+background = None
+
+ret, frame = camera.read()
+background = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+background = cv2.GaussianBlur(background, (11, 11), 0)
+
+while (True):
+    ret, frame = camera.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.GaussianBlur(gray_frame, (11, 11), 0)
+
+    diff = cv2.absdiff(background, gray_frame)
+    diff = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
+    diff = cv2.dilate(diff, es, iterations = 2)
+    image, cnts, hierachy = cv2.findContours(diff.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in cnts:
+        if cv2.contourArea(c) < 1500:
+            continue
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("contours", frame)
+    cv2.imshow("diff", diff)
+    if cv2.waitKey((int)(1000 / 12)) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+camera.release()
+
