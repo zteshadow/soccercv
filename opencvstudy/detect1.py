80744dfe27f0a571919f696283457c301a2cddcb
zteshadow <samuel.song.bc@gmail.com>
2018-07-30 06:16:43 +0800

    add debug info

--- opencvstudy/detect1.py
+++ opencvstudy/detect1.py
@@ -7,14 +7,28 @@ es = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 4))
 kernel = np.ones((5, 5), np.uint8)
 background = None
 
-ret, frame = camera.read()
+#drop unstable frames at very first
+for i in range(1):
+    ret, frame = camera.read()
+
+#get background frame
+ret, frame = camera.read()    
 background = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-background = cv2.GaussianBlur(background, (11, 11), 0)
+background = cv2.GaussianBlur(background, (21, 21), 0)
+
+#debug first frame and background frame
+'''
+cv2.imshow('first', frame)
+cv2.imshow('background', background)
+while (True):
+    if cv2.waitKey((int)(1000 / 12)) & 0xFF == ord('q'):
+        exit(0)
+'''
 
 while (True):
     ret, frame = camera.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray_frame = cv2.GaussianBlur(gray_frame, (11, 11), 0)
+    gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
     diff = cv2.absdiff(background, gray_frame)
     diff = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
