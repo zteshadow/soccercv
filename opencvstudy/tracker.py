c31ae4709e4bf08fea5e4315bb39e10b0ff3f221
zteshadow <samuel.song.bc@gmail.com>
2018-09-07 06:58:42 +0800

    start test

--- opencvstudy/tracker.py
+++ opencvstudy/tracker.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == '__main__':
     tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
-    tracker_type = tracker_types[2]
+    tracker_type = tracker_types[1]
     
     if int(major_ver) < 3:
         tracker = cv2.Tracker_create(tracker_type)
@@ -38,15 +38,29 @@ if __name__ == '__main__':
             print('read video file error')
             sys.exit()
 
-        bbox = (710, 303, 20, 20)
+        #bbox = (710, 303, 20, 20)
         bbox = cv2.selectROI(frame, False)
 
-        p1 = (int(bbox[0]), int(bbox[1]))
-        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-        cv2.rectangle(frame, p1, p2, (0, 0, 255), 2, 1)
+        ok = tracker.init(frame, bbox)
 
-        cv2.imshow('Frame', frame)
+        while (True):
+            ok, frame = video.read()
+            if not ok:
+                break
+            timer = cv2.getTickCount()
+            ok, bbox = tracker.update(frame)
+            fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+            if ok:
+                p1 = (int(bbox[0]), int(bbox[1]))
+                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                cv2.rectangle(frame, p1, p2, (0, 0, 255), 2, 1)
+            else:
+                cv2.putText(frame, 'Tracking failure detected', (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+            
+            cv2.putText(frame, tracker_type + ' Tracker', (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+            cv2.putText(frame, 'FPS: ' + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+            cv2.imshow('Frame', frame)
 
-        if cv2.waitKey(0) & 0xFF == ord('q'):
-            sys.exit()
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
