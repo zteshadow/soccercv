a5752d97f3a85d9ae0ab5da496da4b2002b5bcd1
zteshadow <samuel.song.bc@gmail.com>
2018-09-08 16:11:19 +0800

    cpp test ok

--- opencvstudy/cpp/test/test/main.cpp
+++ opencvstudy/cpp/test/test/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/videoio/videoio.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <opencv2/core/core.hpp>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace cv;
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    VideoCapture cap(0);
+    namedWindow("frame", cv::WINDOW_NORMAL);
+    
+    while(true)
+    {
+        Mat frame;
+        cap.read(frame);
+        if(frame.empty())
+        {
+            cout << "No frame" << endl;
+        }
+        imshow("frame", frame);
+        
+        int key = cv::waitKey(1);
+        if ((key & 0xFF) == 'q')
+        {
+            break;
+        }
+    }
+    
     return 0;
 }
