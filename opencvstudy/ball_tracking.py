1d01a8c26fe8d30740a5a08d479531d445f0a4e8
zteshadow <samuel.song.bc@gmail.com>
2018-09-27 06:01:43 +0800

    test color based ball detection

--- opencvstudy/ball_tracking.py
+++ opencvstudy/ball_tracking.py
@@ -31,9 +31,37 @@ while True:
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
+    #locate the ball by color
     mask = cv2.inRange(hsv, greenLower, greenUpper)
     mask = cv2.erode(mask, None, iterations = 2)
     mask = cv2.dilate(mask, None, iterations = 2)
+
+    #find contours
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    center = None
+
+    #circle
+    if len(cnts) > 0:
+        c = max(cnts, key = cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c) #矩
+        center = (int(M["m10"] / M["m00"]), int(M['m01'] / M['m00']))
+
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+    # track 
+    pts.appendleft(center)
+    
+    for i in range(1, len(pts)):
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+
+        thickness = int(np.sqrt(args['buffer'] / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+    cv2.imshow('frame', frame)
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
@@ -45,4 +73,3 @@ else:
     vs.release()
 
 cv2.destroyAllWindows()
-
