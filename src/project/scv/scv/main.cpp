3f8a741cb0513cb2234ca4bac6f33c1256388caf
zteshadow <samuel.song.bc@gmail.com>
2018-11-25 17:51:17 +0800

    add mac project for scv

---
+++ src/project/scv/scv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  scv
+//
+//  Created by samuelsong on 2018/11/25.
+//  Copyright © 2018 samuelsong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
